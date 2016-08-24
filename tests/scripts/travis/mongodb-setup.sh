d80aac5e17a475a61c8931b2b99bccf907e914f4
Victor Gonzalez <victor.gonzalezro@gmail.com>
2016-08-24 17:13:46 -0300

    enable mongo extensions in travis
    Signed-off-by: Victor Gonzalez <victor.gonzalezro@gmail.com>

--- tests/scripts/travis/mongodb-setup.sh
+++ tests/scripts/travis/mongodb-setup.sh
@@ -1,15 +1,5 @@
 #!/bin/sh -e
 #
-# install mongodb
 
-# MongoDB Server :
-
-echo "MongoDB Server version:"
-mongod --version
-
-# PHP Extension :
-if (php --version | grep -i 7.0 > /dev/null); then
-  echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-else
-  echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-fi
+echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
