5a5bf222449740d8d12925285b5ae5fa278d0e93
Victor Gonzalez <victor.gonzalez@fch.cl>
2016-08-30 12:48:31 -0300

    fix travis error
    Signed-off-by: Victor Gonzalez <victor.gonzalez@fch.cl>

--- tests/scripts/travis/mongodb-setup.sh
+++ tests/scripts/travis/mongodb-setup.sh
@@ -2,12 +2,7 @@
 
 echo $(phpenv version-name)
 
-# PHP Extension :
-if [ $(phpenv version-name) = "7.0" ]; then
-  echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-else
-  pecl install -f mongodb
-  echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-  echo "MongoDB PHP Extension version:"
-  php -i |grep mongodb -4 |grep -2 version
-fi
+# PHP MongoDB Extension
+echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "MongoDB PHP Extension version:"
+php -i |grep mongodb -4 |grep -2 version
