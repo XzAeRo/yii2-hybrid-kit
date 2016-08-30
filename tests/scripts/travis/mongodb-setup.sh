226b21f6cb16765f8ac4e3ca048e54959ab371a4
Victor Gonzalez <victor.gonzalez@fch.cl>
2016-08-30 12:37:08 -0300

    add pecl for mongodb
    Signed-off-by: Victor Gonzalez <victor.gonzalez@fch.cl>

--- tests/scripts/travis/mongodb-setup.sh
+++ tests/scripts/travis/mongodb-setup.sh
@@ -6,5 +6,8 @@ echo $(phpenv version-name)
 if [ $(phpenv version-name) = "7.0" ]; then
   echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 else
-  echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+  pecl install -f mongodb
+  echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+  echo "MongoDB PHP Extension version:"
+  php -i |grep mongodb -4 |grep -2 version
 fi
